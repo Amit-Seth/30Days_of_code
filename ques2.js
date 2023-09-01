85f6205ea610a91bfe004039c3d4548a3773df6c
Amit-Seth <137941510+Amit-Seth@users.noreply.github.com>
2023-09-01 23:56:19 +0530

    Add files via upload

---
+++ ques2.js
@@ -0,0 +1,11 @@
+function reverseString(String) {
+    let reverse = '';
+    for (let i = String.length - 1; i >= 0; i--) {
+      reverse += String[i];
+    }
+    return reverse;
+  }
+  
+  const myString = "abc";
+  const newString = reverseString(myString);
+  console.log(newString); 
